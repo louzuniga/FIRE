9f660fe93d6bb2683e1ace352127a7fa0ebb4af5
Lou Zuniga <louzuniga86@gmail.com>
2019-04-21 22:33:04 -0700

    modified questions

--- public/questions.js
+++ public/questions.js
@@ -7,6 +7,7 @@ const questions = [
             'a.	All of the basics are taken care of such as housing, healthcare, food, minimal travel and entertainment and more time for myself and my family',
             'b.	All of questions above but would include more for entertainment, travel and the nicer materialistic things in life',
             'c.	All of the above and then everything else I can possibly want without limitations',
+            'd. Undecided',
         ],
     },
     {
@@ -15,6 +16,7 @@ const questions = [
             'a.	I live paycheck to paycheck and/or barely getting by',
             'b.	I save about 20% of my salary (includes 401k contribution)',
             'c.	I am proudly debt free and save more than 20% of my salary (includes 401k contribution)',
+            'd. Undecided',
         ],
     },
     {
@@ -23,6 +25,7 @@ const questions = [
             'a.	Yes, I understand the turbulent ride, but the historical high returns are worth the risk',
             'b.	Somewhat, my risk tolerance is low, and I prefer to invest more in bonds ',
             'c.	No, I prefer to just keep all my money in my savings and/or CD’s.',
+            'd. Undecided',
         ],
     },
     {
@@ -30,7 +33,8 @@ const questions = [
         choices: [
             'a.	Yes, I can identify ways to minimize my spending to my values',
             'b.	No, I can’t identify anything else I can cut back on',
-            'c. Already in progress'
+            'c. Already in progress',
+            'd. Undecided',
         ],
     },
     {
@@ -39,6 +43,7 @@ const questions = [
             'a.	Having a big house, nice car, designer clothes and shoes and lot of money ',
             'b.	Having great friendships and a close relationship with my family',
             'c.	Option b in addition to being able to live freely, travel, and enjoy what I love most in life within cognizant considerations',
+            'd. Undecided',
         ],
     },
 ];
@@ -69,6 +74,11 @@ const generateQuestions = () => {
             <div class="answerChoices">
             <input type="radio" name="choice" required></input>
             <label>${questions[numberOfQuestions].choices[2]}</label>
+            </div>
+
+            <div class="answerChoices">
+            <input type="radio" name="choice" required></input>
+            <label>${questions[numberOfQuestions].choices[3]}</label>
             </div>
             </br>
             <button id="next-question" class="question-btn btn">Next</button>
